d104a758c4566c969445c8fcb2e4c00ea43e1fc6
EnesKilicaslan <eeneskilicaslan@gmail.com>
2021-06-20 11:16:49 -0700

    Add User Registration Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./pages/LoginForm";
+import RegistrationForm from "./pages/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
       {/* <FormikContainer /> */}
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <RegistrationForm />
     </div>
   );
 }
